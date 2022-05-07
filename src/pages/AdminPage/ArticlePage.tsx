3109911634201a1e31f1fbf8054494c1a560edaa
BryantSuen <BryantSuen@163.com>
2022-05-07 18:52:33 +0800

    feat: :sparkles: add edit articles

--- src/pages/AdminPage/ArticlePage.tsx
+++ src/pages/AdminPage/ArticlePage.tsx
@@ -1,61 +1,128 @@
-import React from "react";
-import { Breadcrumb, List, Button, Divider } from "antd";
+import React, { useEffect, useState } from "react";
+import {
+  Breadcrumb,
+  List,
+  Button,
+  Divider,
+  message,
+  Modal,
+  Input,
+  Form,
+  Row,
+} from "antd";
+import { ExclamationCircleOutlined } from "@ant-design/icons";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { useForm } from "antd/lib/form/Form";
+import ReactMarkdown from "react-markdown";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+import "katex/dist/katex.min.css";
 
+const { confirm } = Modal;
+interface Article {
+  _id: string;
+  title: string;
+  description: string;
+  updatedAt: Date;
+  author: string;
+  content: string;
+}
 const ArticlePage = () => {
-  const handleEditArticle = () => {};
-  const handleDeleteArticle = () => {};
-  const data = [
-    {
-      title: "article1",
-      author: "bryantsuen",
-      time: "2022-5-4",
-      abstract: "haha",
-      _id: "123456",
-    },
-    {
-      title: "article2",
-      author: "bryantsuen",
-      time: "2022-5-4",
-      abstract: "haha",
-      _id: "123457",
-    },
-    {
-      title: "article3",
-      author: "bryantsuen",
-      time: "2022-5-4",
-      abstract: "haha",
-      _id: "123458",
-    },
-    {
-      title: "article3",
-      author: "bryantsuen",
-      time: "2022-5-4",
-      abstract: "haha",
-      _id: "123458",
-    },
-    {
-      title: "article3",
-      author: "bryantsuen",
-      time: "2022-5-4",
-      abstract: "haha",
-      _id: "123458",
-    },
-    {
-      title: "article3",
-      author: "bryantsuen",
-      time: "2022-5-4",
-      abstract: "haha",
-      _id: "123458",
-    },
-    {
-      title: "article3",
-      author: "bryantsuen",
-      time: "2022-5-4",
-      abstract: "haha",
-      _id: "123458",
-    },
-  ];
+  const [editModalVisible, setEditModalVisible] = useState(false);
+  // switch between edit and preview
+  const [editVisible, setEditVisible] = useState(true);
+  const [editArticleForm] = useForm();
+
+  const handleEditArticle = (_id: string) => async () => {
+    try {
+      const res = await axios.get(`/articles/${_id}`);
+      const article: Article = res.data;
+      console.log(article);
+      editArticleForm.setFieldsValue({
+        _id: article._id,
+        title: article.title,
+        description: article.description,
+        content: article.content,
+      });
+    } catch (err) {
+      message.error("refill failed");
+    }
+    setEditModalVisible(true);
+  };
+
+  const handleUpdateArticle = (_id: string) => () => {
+    confirm({
+      title: "Do you Want to Update the article?",
+      icon: <ExclamationCircleOutlined />,
+      content: "This operation will overwrite the previous.",
+      onOk: async () => {
+        try {
+          await axios.put(`/articles/${_id}`, {
+            title: editArticleForm.getFieldValue("title"),
+            description: editArticleForm.getFieldValue("description"),
+            content: editArticleForm.getFieldValue("content"),
+          });
+          message.success("update success");
+        } catch (err) {
+          message.error("update failed");
+          console.log(err);
+        }
+      },
+      onCancel() {
+        console.log("Cancel");
+      },
+    });
+  };
+  const handleDeleteArticle = (_id: string, title: string) => () => {
+    confirm({
+      title: `Are you sure to delete ${title}?`,
+      icon: <ExclamationCircleOutlined />,
+      content: "This opeartion can not be restored!",
+      okText: "Yes",
+      okType: "danger",
+      cancelText: "No",
+      onOk: async () => {
+        try {
+          message.warning("Deleting");
+          await axios.delete(`/articles/${_id}`);
+          message.success("Deleted");
+          try {
+            setLoading(true);
+            const res = await axios.get("/articles");
+            setArticleList(res.data);
+            setLoading(false);
+          } catch (err) {
+            message.error("failed in loading");
+            console.log(err);
+          }
+        } catch (err) {
+          message.error("Delete failed");
+          console.log(err);
+        }
+      },
+      onCancel() {
+        console.log("Cancel");
+      },
+    });
+  };
+
+  const [articleList, setArticleList] = useState([]);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    (async () => {
+      try {
+        setLoading(true);
+        const res = await axios.get("/articles");
+        setArticleList(res.data);
+        setLoading(false);
+      } catch (err) {
+        message.error("failed in loading");
+        console.log(err);
+      }
+    })();
+  }, []);
+
   return (
     <div style={{ height: "100%" }}>
       <Breadcrumb style={{ margin: "16px 0" }}>
@@ -78,35 +145,96 @@ const ArticlePage = () => {
         <Divider />
         <List
           itemLayout="horizontal"
-          dataSource={data}
+          dataSource={articleList}
+          loading={loading}
           pagination={{
             pageSize: 6,
           }}
-          renderItem={(item) => (
+          renderItem={(item: Article) => (
             <List.Item>
               <List.Item.Meta
-                title={
-                  <Link to={`/admin/articles/${item._id}`}>{item.title}</Link>
-                }
-                description={item.abstract}
+                title={<Link to={`/articles/${item._id}`}>{item.title}</Link>}
+                description={item.description}
               />
               <Button
                 type="primary"
                 style={{ margin: "5px" }}
-                onClick={handleEditArticle}
+                onClick={handleEditArticle(item._id)}
               >
                 edit
               </Button>
               <Button
                 danger
                 style={{ margin: "5px" }}
-                onClick={handleDeleteArticle}
+                onClick={handleDeleteArticle(item._id, item.title)}
               >
                 delete
               </Button>
             </List.Item>
           )}
         />
+        <Modal
+          title="Edit"
+          visible={editModalVisible}
+          okText="Save"
+          cancelText="cancel"
+          onOk={handleUpdateArticle(editArticleForm.getFieldValue("_id"))}
+          onCancel={() => {
+            setEditModalVisible(false);
+          }}
+        >
+          {editVisible ? (
+            <Form
+              form={editArticleForm}
+              layout="vertical"
+              autoComplete="off"
+              onFinish={() => {}}
+            >
+              <Form.Item name="title" label="Title">
+                <Input placeholder="Input title" />
+              </Form.Item>
+
+              <Form.Item name="description" label="Description">
+                <Input.TextArea rows={2} placeholder="Input description" />
+              </Form.Item>
+
+              <Form.Item name="content" label="Content (markdown format)">
+                <Input.TextArea rows={8} placeholder="Input content" />
+              </Form.Item>
+            </Form>
+          ) : (
+            <ReactMarkdown
+              remarkPlugins={[remarkMath]}
+              rehypePlugins={[rehypeKatex]}
+            >
+              {"# " +
+                editArticleForm.getFieldValue("title") +
+                "\n" +
+                editArticleForm.getFieldValue("content")}
+            </ReactMarkdown>
+          )}
+          <Row>
+            <Button
+              type="primary"
+              htmlType="button"
+              style={{ margin: "10px", width: "80px" }}
+              onClick={() => {
+                setEditVisible(!editVisible);
+              }}
+            >
+              {editVisible ? "Preview" : "Edit"}
+            </Button>
+            <Button
+              htmlType="button"
+              style={{ margin: "10px" }}
+              onClick={() => {
+                editArticleForm.resetFields();
+              }}
+            >
+              Reset
+            </Button>
+          </Row>
+        </Modal>
       </div>
     </div>
   );
